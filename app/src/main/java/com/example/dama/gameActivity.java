4d7fdbdaa7609947b2f3ef551b653bfca6027a9b
micaigulag <micaigiulio@gmail.com>
2024-05-07 12:21:53 +0200

    campo di gioco e drawer menu

--- app/src/main/java/com/example/dama/gameActivity.java
+++ app/src/main/java/com/example/dama/gameActivity.java
@@ -8,6 +8,6 @@ public class gameActivity extends AppCompatActivity  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.partita);
+        setContentView(R.layout.partita2);
     }
 }
