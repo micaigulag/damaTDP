fb94397ac358d2fe17d2969dce0fce5b78d24196
micaigulag <micaigiulio@gmail.com>
2024-05-07 12:44:08 +0200

    creazione scacchira trasparente

--- app/src/main/java/com/example/dama/campoDiGioco.java
+++ app/src/main/java/com/example/dama/campoDiGioco.java
@@ -13,7 +13,8 @@ import android.view.View;
 public class campoDiGioco extends View {
 
     private Paint paint = new Paint();
-
+    private final int MARGIN = 5;
+    private final int BORDI = 20;
     public campoDiGioco(Context context) {
         super(context);
     }
@@ -31,7 +32,13 @@ public class campoDiGioco extends View {
         super.onDraw(canvas);
 
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.scacchiera);
-        canvas.drawBitmap(Bitmap.createScaledBitmap(bitmap,1070, 1070,false ) , 5, 5, null);
+        canvas.drawBitmap(Bitmap.createScaledBitmap(bitmap,1070, 1070,false ) , MARGIN, MARGIN, null);
+
+        paint.setColor(Color.GREEN);
+        canvas.drawRect((MARGIN + BORDI),(MARGIN + BORDI),120,120,paint);
+
+        paint.setColor(Color.RED);
+        canvas.drawRect((MARGIN + BORDI)+100,(MARGIN + BORDI),200,200,paint);
 
     }
 }
